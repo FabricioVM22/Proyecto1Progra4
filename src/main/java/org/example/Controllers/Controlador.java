f0d8ca87784127b92ba25c781fa479115c862bc5
FabricioVM <72674763+FabricioVM22@users.noreply.github.com>
2024-03-25 23:21:51 -0600

    Primer boceto de las paginas

--- src/main/java/org/example/Controllers/Controlador.java
+++ src/main/java/org/example/Controllers/Controlador.java
@@ -1,4 +1,5 @@
 package org.example.Controllers;
+
 import org.example.Models.Modelo;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -6,20 +7,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @org.springframework.stereotype.Controller
 public class Controlador {
-     @GetMapping("/")
-     public String index(Model model){
-         //aquí debería haber una conexion a la capa de logica que les traiga los datos por ejemplo de las facturas
-         Modelo modelo1 =new Modelo("Esta es la pagina de inicio del sistema :D");
-         model.addAttribute("modelo", modelo1);
-         return "index";
-     }
+    @GetMapping("/")
+    public String index(Model model) {
+        //aquí debería haber una conexion a la capa de logica que les traiga los datos por ejemplo de las facturas
+        Modelo modelo1 = new Modelo("Esta es la pagina de inicio del sistema :D");
+        model.addAttribute("modelo", modelo1);
+        return "index";
+    }
+
+    @GetMapping("/RegistrarClientes")
+    public String RegistrarCliente(Model model) {
+        Modelo modeloRC = new Modelo("esta es la pagina de registro de Clientes");
+        model.addAttribute("modelo", modeloRC);
+        return "RegistrarClientes";
+    }
+
+    @GetMapping("/RegistrarFacturas")
+    public String RegistrarFacturas(Model model) {
+        Modelo modeloRF = new Modelo("esta es la pagina de información");
+        model.addAttribute("modelo", modeloRF);
+        return "RegistrarFacturas";
+    }
 
 
-     @GetMapping("/about")
-    public String about(Model model){
-         Modelo modelo =new Modelo("esta es la pagina de información");
-         model.addAttribute("modelo",modelo);
-         return "about";
-     }
+    @GetMapping("/about")
+    public String about(Model model) {
+        Modelo modelo = new Modelo("esta es la pagina de información");
+        model.addAttribute("modelo", modelo);
+        return "about";
+    }
 
 }
