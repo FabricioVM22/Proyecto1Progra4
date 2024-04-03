1970c80d8f1f5553470e823fde28d2bcaa5dd5a8
Gabogapp <subarugabogapp@gmail.com>
2024-04-03 17:45:05 -0600

    Cambio del application

--- src/main/java/org/example/Proyecto1Progra4Application.java
+++ src/main/java/org/example/Proyecto1Progra4Application.java
@@ -3,7 +3,10 @@ package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "org.example.repository")
 public class Proyecto1Progra4Application {
 
     public static void main(String[] args) {
