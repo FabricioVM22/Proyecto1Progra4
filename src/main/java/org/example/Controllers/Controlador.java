d14f20f0398101d34b8bb33506256afcdcbfbbb2
Gabogapp <subarugabogapp@gmail.com>
2024-04-10 21:12:23 -0600

    Actualizar Getsmapping

--- src/main/java/org/example/Controllers/Controlador.java
+++ src/main/java/org/example/Controllers/Controlador.java
@@ -26,6 +26,8 @@ public class Controlador {
     //Bases de datos para Usuarios
     @Autowired
     private UsuarioRepository usuarioRepository;
+    Usuario usuarioActual;
+
     @PostMapping(path="addUsuario")
     public @ResponseBody String addNuevoUsuario(@RequestParam String nombre, @RequestParam String email){
 
@@ -118,6 +120,7 @@ public class Controlador {
             }
             if(nUsuario.getContrasena().equals(clave)){
                 model.addAttribute("usuario",nUsuario);
+                usuarioActual = nUsuario;
                 return "Perfil";
             }
         }catch (Exception ex){}
@@ -127,35 +130,33 @@ public class Controlador {
     /*paginas base*/
     @GetMapping("/")
     public String index(Model model) {
-        //aquí debería haber una conexion a la capa de logica que les traiga los datos por ejemplo de las facturas
-        Modelo modelo1 = new Modelo("Esta es la pagina de inicio del sistema :D");
-        model.addAttribute("modelo", modelo1);
-        return "index";
+        usuarioActual = new Usuario();
+        return "Index";
     }
 
     @GetMapping("/Clientes")
     public String Clientes(Model model) {
-        Modelo modeloC = new Modelo("esta es la pagina de vista de Clientes");
-        model.addAttribute("modelo", modeloC);
+        Iterable<Cliente> clientes = clienteRepository.findAll();
+        model.addAttribute("clientes",clientes);
         return "Clientes";
     }
 
     @GetMapping("/Facturas")
     public String Facturas(Model model) {
-        Modelo modeloF = new Modelo("esta es la pagina de vista de Facturas");
-        model.addAttribute("modelo", modeloF);
+        Iterable<Factura> facturas = facturaRepository.findAll();
+        model.addAttribute("facturas",facturas);
         return "Facturas";
     }
     @GetMapping("/Productos")
     public String Productos(Model model) {
-        Modelo modeloP = new Modelo("esta es la pagina de vista de Productos");
-        model.addAttribute("modelo", modeloP);
+        Iterable<Producto> productos = productoRepository.findAll();
+        model.addAttribute("productos",productos);
         return "Productos";
     }
     @GetMapping("/Administracion")
     public String Administracion(Model model) {
-        Modelo modeloA = new Modelo("esta es la pagina de administracion");
-        model.addAttribute("modelo", modeloA);
+        Iterable<Usuario> usuarios = usuarioRepository.findAll();
+        model.addAttribute("usuarios",usuarios);
         return "Administracion";
     }
     /*forms*/
@@ -184,18 +185,12 @@ public class Controlador {
         model.addAttribute("modelo", modeloAP);
         return "AdministrarProveedores";
     }
-    /*about*/
-    @GetMapping("/about")
-    public String about(Model model) {
-        Modelo modelo = new Modelo("esta es la pagina de información");
-        model.addAttribute("modelo", modelo);
-        return "about";
-    }
     @GetMapping("/Perfil")
     public String perfil(Model model) {
         Modelo modelo = new Modelo("esta es la pagina de información");
+        model.addAttribute("usuario",usuarioActual);
         model.addAttribute("modelo", modelo);
-        return "about";
+        return "Perfil";
     }
 
 }
