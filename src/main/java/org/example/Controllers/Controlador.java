a3e2a69797d2d34d483a85b1df907af94e5f380f
FabricioVM <72674763+FabricioVM22@users.noreply.github.com>
2024-05-15 21:44:33 -0600

    priner cambio a controller

--- src/main/java/org/example/Controllers/Controlador.java
+++ src/main/java/org/example/Controllers/Controlador.java
@@ -1,98 +1,37 @@
 package org.example.Controllers;
 
 import org.example.Models.*;
-import org.example.repository.ClienteRepository;
-import org.example.repository.FacturaRepository;
-import org.example.repository.ProductoRepository;
-import org.example.repository.UsuarioRepository;
-
+import org.example.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.Iterator;
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
 
 @Controller
-@RequestMapping(path = "/")
 public class Controlador {
-    //Bases de datos para Usuarios
+    private final UsuarioRepository usuarioRepository;
+    private final ClienteRepository clienteRepository;
+    private FacturaRepository facturaRepository;
+    private ProductoRepository productoRepository;
+    private Usuario usuarioActual;
+
     @Autowired
-    private UsuarioRepository usuarioRepository;
-    Usuario usuarioActual;
-
-    @PostMapping(path = "addUsuario")
-    public String addNuevoUsuario(@RequestParam("nombre") String nombre,@RequestParam("clave") String clave, @RequestParam("correo") String email) {
-
-        Usuario nuevoUsuario = new Usuario();
-        nuevoUsuario.setNombre(nombre);
-        nuevoUsuario.setContrasena(clave);
-        nuevoUsuario.setEmail(email);
-        nuevoUsuario.setEstado(false);
-        usuarioRepository.save(nuevoUsuario);
-        return "index";
+    public Controlador(UsuarioRepository usuarioRepository, ClienteRepository clienteRepository, FacturaRepository facturaRepository, ProductoRepository productoRepository) {
+        this.usuarioRepository = usuarioRepository;
+        this.clienteRepository = clienteRepository;
+        this.facturaRepository = facturaRepository;
+        this.productoRepository = productoRepository;
     }
 
+
+    //cargar tablas
     @GetMapping(path = "listUsuario")
     public String getAllUsers(Model model) {
         Iterable<Usuario> usuarios = usuarioRepository.findAll();
         model.addAttribute("usuarios", usuarios);
         return "Administracion";
-    }
-
-    @PostMapping(path = "/actualizarUsuarios")
-    public String actualizarUsuarios(@RequestParam("estadoUsuario") String estadosUsuarios, Model model) {
-
-
-        // Obtener todos los usuarios del repositorio
-        Iterable<Usuario> usuarios = usuarioRepository.findAll();
-
-        // Dividir el String de estados en un array de Strings
-        String[] estadosArray = estadosUsuarios.split(",");
-
-        // Iterar sobre cada usuario y asignarle su respectivo estado
-        Iterator<Usuario> usuarioIterator = usuarios.iterator();
-        int index = 0;
-        while (usuarioIterator.hasNext() && index < estadosArray.length) {
-            Usuario usuario = usuarioIterator.next();
-            String estadoStr = estadosArray[index].trim(); // Limpiar espacios en blanco
-            boolean estado = Boolean.parseBoolean(estadoStr); // Convertir a boolean
-            usuario.setEstado(estado);
-            usuarioRepository.save(usuario);
-            index++;
-        }
-
-        // Añadir los usuarios actualizados al modelo
-        model.addAttribute("usuarios", usuarios);
-        base(model);
-        // Devolver la vista deseada
-        return "Administracion";
-    }
-
-    //Bases de datos para Clientes
-    @Autowired
-    private ClienteRepository clienteRepository;
-
-    @PostMapping(path = "addCliente")
-    public String addNuevoCliente(@RequestParam("nombre") String nombre, @RequestParam("tipo_cedula") String tipoCedula, @RequestParam("correo") String email, Model model) {
-
-        Cliente nuevoCliente = new Cliente();
-        nuevoCliente.setNombre(nombre);
-        nuevoCliente.setTipoCedula(tipoCedula);
-        nuevoCliente.setEmail(email);
-        clienteRepository.save(nuevoCliente);
-        Iterable<Cliente> clientes = clienteRepository.findAll();
-        model.addAttribute("clientes", clientes);
-        base(model);
-        return "Clientes";
     }
 
     @GetMapping(path = "listCliente")
@@ -102,23 +41,6 @@ public class Controlador {
         return "Clientes";
     }
 
-    //Bases de datos para Facturas
-    @Autowired
-    private FacturaRepository facturaRepository;
-
-    @PostMapping(path = "addFactura")
-    public String addNuevaFactura(@RequestParam("cliente") String cliente, @RequestParam("correo") String email, Model model  ) {
-
-        Factura nuevaFactura = new Factura();
-        nuevaFactura.setCliente(cliente);
-        nuevaFactura.setEmail(email);
-        facturaRepository.save(nuevaFactura);
-        Iterable<Factura> facturas = facturaRepository.findAll();
-        model.addAttribute("facturas", facturas);
-        base(model);
-        return "Facturas";
-    }
-
     @GetMapping(path = "listFacturad")
     public String getAllFacturas(Model model) {
         Iterable<Factura> facturas = facturaRepository.findAll();
@@ -126,28 +48,50 @@ public class Controlador {
         return "Facturas";
     }
 
-    //Bases de datos para Productos
-    @Autowired
-    private ProductoRepository productoRepository;
-
-    @PostMapping(path = "addProducto")
-    public String addNuevoProducto(@RequestParam("descripcion") String descripcion, @RequestParam("cantidad") int cantidad, Model model) {
-
-        Producto nuevoProducto = new Producto();
-        nuevoProducto.setDescripcion(descripcion);
-        nuevoProducto.setCantidad(cantidad);
-        productoRepository.save(nuevoProducto);
-        Iterable<Producto> productos = productoRepository.findAll();
-        model.addAttribute("productos", productos);
-        base(model);
-        return "Productos";
-    }
-
     @GetMapping(path = "listProductos")
     public String getAllProductos(Model model) {
         Iterable<Producto> productos = productoRepository.findAll();
         model.addAttribute("productos", productos);
         return "Producto";
+    }
+
+
+
+    //métodos
+    @PostMapping(path = "addUsuario")
+    public String addNuevoUsuario(Usuario usuario, Model model) {
+        usuarioRepository.save(usuario);
+        Iterable<Usuario> usuarios = usuarioRepository.findAll();
+        model.addAttribute("usuarios", usuarios);
+        base(model);                                                                    //?
+        return "redirect:/index";
+    }
+
+    @PostMapping(path = "addCliente")
+    public String addNuevoCliente(Cliente cliente, Model model) {
+        clienteRepository.save(cliente);
+        Iterable<Cliente> clientes = clienteRepository.findAll();
+        model.addAttribute("clientes", clientes);
+        base(model);
+        return "redirect:/Clientes";
+    }
+
+    @PostMapping(path = "addFactura")
+    public String addNuevaFactura(Factura factura, Model model) {
+        facturaRepository.save(factura);
+        Iterable<Factura> facturas = facturaRepository.findAll();
+        model.addAttribute("facturas", facturas);
+        base(model);
+        return "redirect:/Facturas";
+    }
+
+    @PostMapping(path = "addProducto")
+    public String addNuevoProducto(Producto producto, Model model) {
+        productoRepository.save(producto);
+        Iterable<Producto> productos = productoRepository.findAll();
+        model.addAttribute("productos", productos);
+        base(model);
+        return "Productos";
     }
 
     @PostMapping(path = "/login")
@@ -171,7 +115,7 @@ public class Controlador {
         return "redirect:/";
     }
 
-    /*paginas base*/
+    //cargar paginas
     @GetMapping("/")
     public String index(Model model) {
         usuarioActual = new Usuario();
@@ -235,6 +179,7 @@ public class Controlador {
         base(model);
         return "RegistrarProductos";
     }
+
     @GetMapping("/RegistrarUsuarios")
     public String RegistrarUsuarios(Model model) {
         Modelo modeloRP = new Modelo("esta es la pagina de información");
@@ -259,15 +204,17 @@ public class Controlador {
         base(model);
         return "Perfil";
     }
+
     public void base(Model model) {
         Modelo modelo = new Modelo();
         try {
-            if(usuarioActual.getNombre().equals("admin")){
+            if (usuarioActual.getNombre().equals("admin")) {
                 modelo.setMessage("Administracion");
-            }else{
+            } else {
                 modelo.setMessage(null);
             }
-        }catch (Exception ex){}
+        } catch (Exception ex) {
+        }
 
         model.addAttribute("modelo", modelo);
     }
